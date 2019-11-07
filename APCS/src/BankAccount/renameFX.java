b46a9c7b277afbd85f4ddd10c8f29bd4bf4056a3
BobdaFett <lvas463@gmail.com>
2019-11-06 21:40:24 -0500

    Update renameFX.java
    Started adding features to the rename button

--- APCS/src/BankAccount/renameFX.java
+++ APCS/src/BankAccount/renameFX.java
@@ -1,8 +1,35 @@
 package BankAccount;
 
-public class renameFX {
+import javafx.stage.*;
+import javafx.scene.*;
+import javafx.scene.control.Button;
+import javafx.scene.layout.HBox;
 
+public class renameFX {
+	
+	private static Stage ren;
+	private static Scene renSC;
+	
 	public static void open() {
+		ren = new Stage();
+		
+		Button b1 = new Button("Create a new account");
+		b1.setOnAction(e -> {
+			
+		});
+		
+		Button b2 = new Button("Back to menu...");
+		b2.setOnAction(e -> {
+			ren.close();
+		}); 
+		renSC = new Scene(new HBox(b1, b2));
+		
+		ren.setScene(renSC);
+		
+		ren.show();
+	}
+	
+	public static void rename() {
 		
 	}
 	
