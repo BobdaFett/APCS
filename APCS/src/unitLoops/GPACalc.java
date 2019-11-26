18f6b742e18ef88d044a7d2d397232de04917ee8
BobdaFett <lvas463@gmail.com>
2019-11-26 10:10:22 -0500

    yeet

--- APCS/src/unitLoops/GPACalc.java
+++ APCS/src/unitLoops/GPACalc.java
@@ -4,13 +4,17 @@ import java.util.ArrayList;
 import java.util.List;
 
 import javafx.application.*;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.geometry.Insets;
 import javafx.scene.*;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextArea;
 import javafx.scene.input.KeyCode;
 import javafx.scene.control.Alert.AlertType;
+import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
 import javafx.scene.text.Text;
 import javafx.stage.*;
@@ -23,9 +27,9 @@ import javafx.stage.*;
 
 public class GPACalc extends Application {
 	
-	public static List<String> classes;
-	public static List<Double> credits;
-	public static List<Double> grade;
+	public static ArrayList<String> classes;
+	public static ArrayList<Double> credits;
+	public static ArrayList<Double> grade;
 	public static int count;
 	
 	public void start(Stage s) throws Exception {
@@ -35,18 +39,18 @@ public class GPACalc extends Application {
 		grade = new ArrayList<>();
 		
 		Button b1 = new Button("Add a class");
-		b1.setOnAction(e -> {
-			createClass();
-		});
+		b1.setOnAction(e -> { createClass(); });
 		
-		Button b2 = new Button("Close window");
-		b2.setOnAction(e -> {
-			s.close();
-		});
+		Button b2 = new Button("Calculate GPA");
+		b2.setOnAction(e -> { calc(); });
+		
+		Button b3 = new Button("Close window");
+		b2.setOnAction(e -> { s.close(); });
 		
 		GridPane gp = new GridPane();
 		gp.add(b1, 0, 0);
 		gp.add(b2, 1, 0);
+		gp.add(b3, 0, 1);
 		
 		gp.setPadding(new Insets(15));
 		gp.setVgap(10);
@@ -68,136 +72,40 @@ public class GPACalc extends Application {
 		Stage s = new Stage();
 		Button b1 = new Button("Submit");
 		b1.setDefaultButton(true);
-		TextArea ta = new TextArea();
-		Text t = new Text("Enter a class below: ");
-		
-		ta.setOnKeyPressed(keyEvent -> {
-			if(keyEvent.getCode() == KeyCode.ENTER) {
-				//DO STUFFFFFF
-			}
-		});
-		
-		b1.setOnAction(e -> {
-			try {
-				String temp = ta.getText();
-				classes.add(temp);
-				s.close();
-				enterGrade();
-			} catch(Exception f) {
-				errorWindow("UNKNOWN ERROR", "That didn't work - try again.");
-			}
-		});
-		
-		GridPane gp = new GridPane();
-		gp.add(t, 0, 0);
-		gp.add(ta, 0, 1);
-		gp.add(b1, 1, 1);
-		
-		gp.setPadding(new Insets(10));
-		gp.setVgap(10);
-		gp.setHgap(10);
-		
-		Scene sc = new Scene(gp);
-		
-		s.setScene(sc);
-		s.show();
-	}
-	
-	public void enterGrade() {
-		Stage s = new Stage();
-		Button b1 = new Button("Submit");
-		b1.setDefaultButton(true);
-		TextArea ta = new TextArea();
-		Text t = new Text("Enter your letter grade below: ");
+		Text t = new Text("Select your class\'s information: ");
+		ObservableList<Object> classOptions = FXCollections.observableArrayList("Math", "Science", "English", "History/Social Studies", "Civics");
+		ObservableList<Object> gradeOptions = FXCollections.observableArrayList("A", "B", "C", "D", "F");
+		ObservableList<Object> lengthOptions = FXCollections.observableArrayList("Half", "Full");
+		ComboBox classesBox = new ComboBox(classOptions);
+		ComboBox gradesBox = new ComboBox(gradeOptions);
+		ComboBox lengthBox = new ComboBox(lengthOptions);
 		
 		b1.setOnAction(e -> {
 			try {
-				switch (ta.getText().toUpperCase()) {
-				case "A":
-					grade.add(4.0);
-					s.close();
-					enterLength();
-					break;
-				case "B":
-					grade.add(3.0);
-					s.close();
-					enterLength();
-					break;
-				case "C":
-					grade.add(2.0);
-					s.close();
-					enterLength();
-					break;
-				case "D":
-					grade.add(1.0);
-					s.close();
-					enterLength();
-					break;
-				case "F": 
-					grade.add(0.0);
-					s.close();
-					enterLength();
-					break;
-				default:
-					if(ta.getText().length() > 1) {
-						errorWindow("TOO MANY LETTERS", "Make sure you enter a single letter - more than one will not work.");
-					} else {
-						errorWindow("UNKNOWN ERROR", "That didn't work - try again.");
-					}
-					ta.clear();
-					break;
-				}
-			} catch(Exception f) {
-				errorWindow("UNKNOWN ERROR", "That didn't work - try again.");
-			}
-		});
-		
-		GridPane gp = new GridPane();
-		gp.add(t, 0, 0);
-		gp.add(ta, 0, 1);
-		gp.add(b1, 1, 1);
-		
-		gp.setPadding(new Insets(10));
-		gp.setVgap(10);
-		gp.setHgap(10);
-		
-		Scene sc = new Scene(gp);
-		
-		s.setScene(sc);
-		s.show();
-	}
-	
-	public void enterLength() {
-		Stage s = new Stage();
-		Button b1 = new Button("Submit");
-		b1.setDefaultButton(true);
-		TextArea ta = new TextArea();
-		Text t = new Text("Enter the length of the class below: \nEnter \"Half\" or \"Full\"");
-		
-		b1.setOnAction(e -> {
-			try {
-				switch(ta.getText().toLowerCase()) {
+				classes.add(classesBox.getSelectionModel().getSelectedItem().toString());
+				grade.add((double) gradesBox.getSelectionModel().getSelectedIndex());
+				switch(gradesBox.getSelectionModel().getSelectedItem().toString().toLowerCase()) {
 				case "half":
-					credits.add(.5);
-					s.close();
+					credits.add(0.5);
 					break;
 				case "full":
 					credits.add(1.0);
-					s.close();
-					break;
-				default:
-					errorWindow("INVALID ARGUMENT", "You didn't enter a vaild length.");
-					ta.clear();
 					break;
 				}
+				s.close();
 			} catch(Exception f) {
 				errorWindow("UNKNOWN ERROR", "That didn't work - try again.");
 			}
 		});
 		
+		
+		ColumnConstraints cc = new ColumnConstraints();
 		GridPane gp = new GridPane();
+		gp.getColumnConstraints().add(cc);
 		gp.add(t, 0, 0);
-		gp.add(ta, 0, 1);
+		gp.add(classesBox, 0, 1);
+		gp.add(gradesBox, 0, 2);
+		gp.add(lengthBox, 0, 3);
 		gp.add(b1, 1, 1);
 		
 		gp.setPadding(new Insets(10));
@@ -210,8 +118,13 @@ public class GPACalc extends Application {
 		s.show();
 	}
 	
+	
 	public void calc() {
-		
+		for(int i = 1; i < credits.size(); i++) {
+			double temp = grade.get(i)/credits.get(i); //grade (total points) divided by credits (total credits possible for that class)
+			System.out.println("You have a " + temp + " GPA in your " + classes.get(i) + " class");
+		}
+		//Eventually make a part here where it calculates your total GPA overall.
 	}
 	
 	public static void errorWindow(String errorType, String message) {
@@ -222,5 +135,3 @@ public class GPACalc extends Application {
 		error.showAndWait();
 	}
 }
-
-
