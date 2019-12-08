49fa296bbf262e5d1c19225207f77fb8f9bf164a
BobdaFett <lvas463@gmail.com>
2019-12-08 16:01:39 -0500

    blep 3

--- APCS/src/unitLoops/GPACalc.java
+++ APCS/src/unitLoops/GPACalc.java
@@ -2,8 +2,6 @@ package unitLoops;
 
 import java.text.DecimalFormat;
 import java.util.ArrayList;
-import java.util.List;
-
 import javafx.application.*;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -12,10 +10,7 @@ import javafx.scene.*;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
-import javafx.scene.control.TextArea;
-import javafx.scene.input.KeyCode;
 import javafx.scene.control.Alert.AlertType;
-import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
 import javafx.scene.text.Text;
 import javafx.stage.*;
@@ -34,7 +29,7 @@ public class GPACalc extends Application {
 	public static ArrayList<Double> gpa;
 	public static int count;
 	
-	public void start(Stage s) throws Exception {
+	public void start(Stage s) throws Exception { //initial window method
 		
 		//initialize the ArrayLists
 		classes = new ArrayList<>();
@@ -42,29 +37,110 @@ public class GPACalc extends Application {
 		grade = new ArrayList<>();
 		gpa = new ArrayList<>();
 		
-		Button b1 = new Button("Add a class");
-		b1.setOnAction(e -> { 
-			createClass(); 
-		});
+		Button addClass = new Button("Add class");
 		
-		Button b2 = new Button("Calculate GPA");
-		b2.setOnAction(e -> { 
+		Button calc = new Button("Calculate GPA");
+		calc.setOnAction(e -> { 
 			calculate(); 
 		});
 		
-		Button b3 = new Button("Close window");
-		b3.setOnAction(e -> { 
-			s.close();
+		//make a text above the options.
+		Text t = new Text("Select your class\'s information: ");
+		
+		//options for the ComboBoxes
+		ObservableList<Object> classOptions = FXCollections.observableArrayList("Math", "Science", "English", "History/Social Studies", "Civics", "Other"); //all possible classes so far.... might import the school catalog and see what i can do with that
+		ObservableList<Object> gradeOptions = FXCollections.observableArrayList("A+", "A", "B+", "B", "C+", "C", "D", "F+", "F"); //added the more complex grades (the "+" grades)
+		ObservableList<Object> lengthOptions = FXCollections.observableArrayList("Half Year", "Full Year"); //possible lengths of the classes
+		
+		//create the ComboBoxes
+		ComboBox<Object> classesBox = new ComboBox<Object>(classOptions);
+		classesBox.getSelectionModel().selectedItemProperty().addListener(e -> { //called any time the user selects something in this box
+			switch(classesBox.getSelectionModel().getSelectedItem().toString()) {
+			case "Other": //create an area for the user to enter their custom class
+				System.out.println("Other");
+				break;
+			}
 		});
 		
-		GridPane gp = new GridPane();
-		gp.add(b1, 0, 0);
-		gp.add(b2, 1, 0);
-		gp.add(b3, 0, 1);
+		ComboBox<Object> gradesBox = new ComboBox<Object>(gradeOptions);
+		ComboBox<Object> lengthBox = new ComboBox<Object>(lengthOptions);
 		
-		gp.setPadding(new Insets(15));
-		gp.setVgap(10);
+		addClass.setOnAction(e -> {
+			try {
+				classes.add(classesBox.getSelectionModel().getSelectedItem().toString()); //get the class name from the corresponding ComboBox
+				System.out.println("Class: " + classesBox.getSelectionModel().getSelectedItem().toString());
+				
+				switch(gradesBox.getSelectionModel().getSelectedItem().toString()) { //get the grade selected from the corresponding ComboBox
+				case "A+":
+					grade.add(4.0); //needed to specify a value for division later
+					System.out.println("Grade: " + gradesBox.getSelectionModel().getSelectedItem().toString()); //really for debugging, but useful until I find a way to make a place that updates for this
+					break;
+				case "A":
+					grade.add(3.5);
+					System.out.println("Grade: " + gradesBox.getSelectionModel().getSelectedItem().toString());
+					break;
+				case "B+":
+					grade.add(3.0);
+					System.out.println("Grade: " + gradesBox.getSelectionModel().getSelectedItem().toString());
+					break;
+				case "B":
+					grade.add(2.5);
+					System.out.println("Grade: " + gradesBox.getSelectionModel().getSelectedItem().toString());
+					break;
+				case "C+":
+					grade.add(2.0);
+					System.out.println("Grade: " + gradesBox.getSelectionModel().getSelectedItem().toString());
+					break;
+				case "C":
+					grade.add(1.5);
+					System.out.println("Grade: " + gradesBox.getSelectionModel().getSelectedItem().toString());
+					break;
+				case "D": 
+					grade.add(1.0);
+					System.out.println("Grade: " + gradesBox.getSelectionModel().getSelectedItem().toString());
+					break;
+				case "F+":
+					grade.add(0.5);
+					System.out.println("Grade: " + gradesBox.getSelectionModel().getSelectedItem().toString());
+					break;
+				case "F":
+					grade.add(0.0);
+					System.out.println("Grade: " + gradesBox.getSelectionModel().getSelectedItem().toString());
+					break;
+				}
+				
+				switch(lengthBox.getSelectionModel().getSelectedItem().toString()) { //get the length of the class from the corresponding ComboBox
+				case "Half Year":
+					credits.add(2.0); //this is because you're dividing - dividing by a decimal makes a larger number.
+					System.out.println("Length: " + lengthBox.getSelectionModel().getSelectedItem().toString() + "\n");
+					break;
+				case "Full Year":
+					credits.add(1.0);
+					System.out.println("Length: " + lengthBox.getSelectionModel().getSelectedItem().toString() + "\n");
+					break;
+				}
+				
+				classesBox.valueProperty().set("");
+				gradesBox.valueProperty().set("");
+				lengthBox.valueProperty().set("");
+				
+			} catch(Exception f) {
+				errorWindow("UNKNOWN ERROR", "That didn't work - try again."); //if there's an error do not allow the user to do anything until they acknowledge it
+			}
+		});
+		
+		GridPane gp = new GridPane(); //create and add everything to a grid layout
+		gp.add(t, 0, 0);
+		gp.add(classesBox, 0, 1);
+		gp.add(gradesBox, 0, 2);
+		gp.add(lengthBox, 0, 3);
+		
+		gp.setPadding(new Insets(10)); //set spacing from sides
+		gp.setVgap(10); //set spacing between each grid area
 		gp.setHgap(10);
+		
+		gp.add(addClass, 2, 1);
+		gp.add(calc, 2, 2);
 		
 		Scene sc = new Scene(gp);
 		
@@ -74,28 +150,34 @@ public class GPACalc extends Application {
 		
 	}
 	
-	
-	public static void main(String[] args) {
+	public static void main(String[] args) { //lauch method
 		Application.launch();
 	}
-	
-	public void createClass() {
+	/*
+	public void createClass() { //class creation, with grades and lengths
 		Stage s = new Stage();
 		
 		//create button, and set so that when you hit enter is clicks it.
 		Button b1 = new Button("Submit");
-		b1.setDefaultButton(true);
 		
 		//make a text above the options.
 		Text t = new Text("Select your class\'s information: ");
 		
 		//options for the ComboBoxes
-		ObservableList<Object> classOptions = FXCollections.observableArrayList("Math", "Science", "English", "History/Social Studies", "Civics");
-		ObservableList<Object> gradeOptions = FXCollections.observableArrayList("A", "B", "C", "D", "F");
-		ObservableList<Object> lengthOptions = FXCollections.observableArrayList("Half", "Full");
+		ObservableList<Object> classOptions = FXCollections.observableArrayList("Math", "Science", "English", "History/Social Studies", "Civics", "Other"); //all possible classes so far.... might import the school catalog and see what i can do with that
+		ObservableList<Object> gradeOptions = FXCollections.observableArrayList("A+", "A", "B+", "B", "C+", "C", "D", "F+", "F"); //added the more complex grades (the "+" grades)
+		ObservableList<Object> lengthOptions = FXCollections.observableArrayList("Half Year", "Full Year"); //possible lengths of the classes
 		
 		//create the ComboBoxes
 		ComboBox<Object> classesBox = new ComboBox<Object>(classOptions);
+		classesBox.getSelectionModel().selectedItemProperty().addListener(e -> { //called any time the user selects something in this box
+			switch(classesBox.getSelectionModel().getSelectedItem().toString()) {
+			case "Other": //create an area for the user to enter their custom class
+				System.out.println("Other");
+				break;
+			}
+		});
+		
 		ComboBox<Object> gradesBox = new ComboBox<Object>(gradeOptions);
 		ComboBox<Object> lengthBox = new ComboBox<Object>(lengthOptions);
 		
@@ -105,20 +187,36 @@ public class GPACalc extends Application {
 				System.out.println("Class: " + classesBox.getSelectionModel().getSelectedItem().toString());
 				
 				switch(gradesBox.getSelectionModel().getSelectedItem().toString()) { //get the grade selected from the corresponding ComboBox
+				case "A+":
+					grade.add(4.0); //needed to specify a value for division later
+					System.out.println("Grade: " + gradesBox.getSelectionModel().getSelectedItem().toString()); //really for debugging, but useful until I find a way to make a place that updates for this
+					break;
 				case "A":
-					grade.add(4.0);
+					grade.add(3.5);
 					System.out.println("Grade: " + gradesBox.getSelectionModel().getSelectedItem().toString());
 					break;
-				case "B": 
+				case "B+":
 					grade.add(3.0);
 					System.out.println("Grade: " + gradesBox.getSelectionModel().getSelectedItem().toString());
 					break;
-				case "C":
+				case "B":
+					grade.add(2.5);
+					System.out.println("Grade: " + gradesBox.getSelectionModel().getSelectedItem().toString());
+					break;
+				case "C+":
 					grade.add(2.0);
 					System.out.println("Grade: " + gradesBox.getSelectionModel().getSelectedItem().toString());
 					break;
-				case "D":
+				case "C":
+					grade.add(1.5);
+					System.out.println("Grade: " + gradesBox.getSelectionModel().getSelectedItem().toString());
+					break;
+				case "D": 
 					grade.add(1.0);
+					System.out.println("Grade: " + gradesBox.getSelectionModel().getSelectedItem().toString());
+					break;
+				case "F+":
+					grade.add(0.5);
 					System.out.println("Grade: " + gradesBox.getSelectionModel().getSelectedItem().toString());
 					break;
 				case "F":
@@ -127,12 +225,12 @@ public class GPACalc extends Application {
 					break;
 				}
 				
-				switch(lengthBox.getSelectionModel().getSelectedItem().toString().toLowerCase()) { //get the length of the class from the corresponding ComboBox
-				case "half":
-					credits.add(2.0);
+				switch(lengthBox.getSelectionModel().getSelectedItem().toString()) { //get the length of the class from the corresponding ComboBox
+				case "Half Year":
+					credits.add(2.0); //this is because you're dividing - dividing by a decimal makes a larger number.
 					System.out.println("Length: " + lengthBox.getSelectionModel().getSelectedItem().toString() + "\n");
 					break;
-				case "full":
+				case "Full Year":
 					credits.add(1.0);
 					System.out.println("Length: " + lengthBox.getSelectionModel().getSelectedItem().toString() + "\n");
 					break;
@@ -149,7 +247,7 @@ public class GPACalc extends Application {
 		gp.add(classesBox, 0, 1);
 		gp.add(gradesBox, 0, 2);
 		gp.add(lengthBox, 0, 3);
-		gp.add(b1, 1, 1);
+		gp.add(b1, 3, 2);
 		
 		gp.setPadding(new Insets(10)); //set spacing from sides
 		gp.setVgap(10); //set spacing between each grid area
@@ -160,10 +258,9 @@ public class GPACalc extends Application {
 		//set scene on stage and then show window
 		s.setScene(sc);
 		s.show();
-	}
+	} */
 	
-	
-	public void calculate() {
+	public void calculate() { //GPA calculation method
 		for(int i = 0; i < credits.size(); i++) {
 			gpa.add(grade.get(i)/credits.get(i)); //grade (total points) divided by credits (total credits possible for that class)
 			
@@ -172,17 +269,17 @@ public class GPACalc extends Application {
 			 * Uses multiple ArrayLists to take the same index for the class, grade, and length, and then associate the GPA with each.
 			 */
 			
-			System.out.println("You have a " + gpa.get(i) + " GPA in your " + classes.get(i) + " class"); //will print the specific gpa and class it's associated with.
+			System.out.println("You have a " + gpa.get(i) + " GPA in your " + classes.get(i) + " class."); //will print the specific gpa and class it's associated with. (Math with a 3.5 GPA)
 		}
 		System.out.println();
 		double sumGPA = 0;
 		for(int i = 0; i < gpa.size(); i++) {
-			sumGPA += gpa.get(i);	//temp variable that stores all GPA's added together
+			sumGPA += gpa.get(i); //temp variable that stores the sum of all GPA's
 		}
 		
-		//returns your average GPA in all of your classes (all GPA's / number of GPA's)
+		//returns your GPA over all your classes (all GPA's / number of GPA's)
 		double overall = sumGPA/gpa.size();
-		DecimalFormat df = new DecimalFormat("#.0"); //allows you to set the number of decimals
+		DecimalFormat df = new DecimalFormat("#.000"); //allows you to set the number of decimals
 		System.out.println("You have a " + df.format(overall) + " GPA on average in your classes.");
 		
 		gpa.clear();
